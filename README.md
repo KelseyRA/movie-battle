5fc4d04b9a71ed6fe6d726f844ab9f18454da44a
Pvon11 <patrickvonatbone@gmail.com>
2023-01-12 20:57:40 -0500

    updated README

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# project-1
+# Movie Battle
+
+## Description
+
+Movie Battle is an intense app that lets two people showcase their knowledge of the entertainment industry by playing a trivia game. The winner gets to pick which movie to watch and their high scores are saved. After the conclusion of the trivia game, the user is provided a search bar to select a variety of movies. the website will dynamically populate the questions and the movies with two seperate api keys that we used. we used Bulma as our CSS Framework. This was a collaborative effort and each developer had a fun time making Movie Battle
+
+## Screenshot
+
+![computer](assets/kelseyra_github_io__movie-battle_.png)
+
+## Live link
+
+[Live Link](https://kelseyra.github.io/movie-battle/)
--- assets/search.js
+++ assets/search.js
@@ -1,19 +1,19 @@
-const apiKeyTMBD = '32ce25589aa56c85a8438a669253213c';
+const apiKeyTMBD = "32ce25589aa56c85a8438a669253213c";
 
+function getApi() {
+  var queryURL =
+    "https://api.themoviedb.org/3/search/movie?api_key=" +
+    apiKeyTMBD +
+    "&query=Jack Reacher";
 
-function getApi(){
-
-    var queryURL = 'https://api.themoviedb.org/3/search/movie?api_key=' + apiKeyTMBD + '&query=Jack Reacher';
-
-    fetch(queryURL)
-        .then(function (response) {
-            data=response.json();
-            return data;
-       })
-        .then(function (data) {
-            console.log(data)
-        })
-
+  fetch(queryURL)
+    .then(function (response) {
+      data = response.json();
+      return data;
+    })
+    .then(function (data) {
+      console.log(data);
+    });
 }
 
 getApi();
